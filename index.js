a4ce1e1ade93d7f59b2c259a874d84b123e35f3f
zeven <fangfan1997@126.com>
2017-03-13 20:20:11 +0800

    export composeWithTracker,ReactiveDict.

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ exports.default = require('./src/Meteor');
 exports.createContainer = require('./src/Meteor').createContainer;
 exports.connectMeteor = require('./src/Meteor').connectMeteor;
 exports.Accounts = require('./src/Meteor').Accounts;
+exports.composeWithTracker = require('./src/Meteor').composeWithTracker;
+exports.ReactiveDict = require('./src/Meteor').ReactiveDict;
